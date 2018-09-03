7fb60b6e4ef873c1ef7eee7a7619978263662acc
Ray Johnson <rayjohnson529@outlook.com>
2018-09-03 17:36:23 -0500

    Update combinedfiles.py

--- combinedfiles.py
+++ combinedfiles.py
@@ -12,11 +12,7 @@ import csv
 import sys
 
 def dir_list(dir_name, subdir, *args):
-    '''Return a list of file names in directory 'dir_name'
-    If 'subdir' is True, recursively access subdirectories under 'dir_name'.
-    Additional arguments, if any, are file extensions to add to the list.
-    Example usage: fileList = dir_list(r'H:\TEMP', False, 'txt', 'py', 'dat', 'log', 'jpg')
-    '''
+
     fileList = []
     for file in os.listdir(dir_name):
         dirfile = os.path.join(dir_name, file)
